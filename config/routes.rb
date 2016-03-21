81dec583a93ccccdfea8f54183b34eae0325ff95
vessp <dwr169@gmail.com>
2016-03-21 16:13:34 +0900

    -server call

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get '/send_url' => 'tunes#send_url'
 
+  get '/open' => 'tunes#open'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
